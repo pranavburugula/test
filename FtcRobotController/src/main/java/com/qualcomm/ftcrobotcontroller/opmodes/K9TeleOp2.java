6697aae048da164b132aba4eb02d03939a20275b
Pranav Burugula <pranav.burugula@gmail.com>
2015-08-15 12:48:29 -0400

    Added Op Mode

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/K9TeleOp2.java
@@ -0,0 +1,76 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by pranavb on 8/15/15.
+ */
+public class K9TeleOp2 extends OpMode {
+    DcMotor motorLeft;
+    DcMotor motorRight;
+     public K9TeleOp2(){}
+    @Override
+    public void init(){
+        motorRight = hardwareMap.dcMotor.get("motor_2");
+        motorLeft = hardwareMap.dcMotor.get("motor_1");
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop(){
+        float throttle2 = -gamepad1.left_stick_y;
+        float direction2 = gamepad1.left_stick_x;
+        float throttle1 = -gamepad1.right_stick_y;
+        float direction1 = gamepad1.right_stick_x;
+        float right = throttle1 - direction1;
+        float left = throttle2 - direction2;
+
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
+
+        right = (float)scaleInput(right);
+        left =  (float)scaleInput(left);
+
+        motorRight.setPower(right);
+        motorLeft.setPower(left);
+
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("left tgt pwr", "left  pwr: " + String.format("%.2f", left));
+        telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+        if (index < 0) {
+            index = -index;
+        } else if (index > 16) {
+            index = 16;
+        }
+
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        return dScale;
+    }
+}
